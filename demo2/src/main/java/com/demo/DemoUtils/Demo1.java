a4431d7e71f6e13147c8380658136ad2762577b2
linkang <jimao08@sina.com>
2016-10-09 20:19:32 +0800

    ddd

--- demo2/src/main/java/com/demo/DemoUtils/Demo1.java
+++ demo2/src/main/java/com/demo/DemoUtils/Demo1.java
@@ -2,11 +2,62 @@ package com.demo.DemoUtils;
 
 import org.apache.commons.lang3.StringUtils;
 
+import java.io.*;
+
 /**
+ * io流demo
  * Created by linkang on 10/9/16.
  */
 public class Demo1 {
     public static void main(String[] args) {
-        System.out.println(StringUtils.isBlank(null));
+
+        mmm();
+    }
+
+
+    public static void mmm() {
+        try {
+            File file = new File("1.txt");
+            FileOutputStream fileOutputStream = new FileOutputStream(file);
+            ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream);
+            MyObject myObject = new MyObject(1, "nba game");
+
+            objectOutputStream.writeObject(myObject);
+            fileOutputStream.close();
+
+
+            ObjectInputStream objectInputStream = new ObjectInputStream(new FileInputStream(file));
+            MyObject mobj = (MyObject)objectInputStream.readObject();
+            System.out.println(mobj.getSval());
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+class MyObject implements Serializable{
+    private int ival;
+    private String sval;
+
+    public MyObject(int ival, String sval) {
+        this.ival = ival;
+        this.sval = sval;
+    }
+
+    public String getSval() {
+        return sval;
+    }
+
+    public void setSval(String sval) {
+        this.sval = sval;
+    }
+
+    public int getIval() {
+        return ival;
+    }
+
+    public void setIval(int ival) {
+        this.ival = ival;
     }
 }
