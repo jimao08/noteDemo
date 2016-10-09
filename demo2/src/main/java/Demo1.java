6a2c517543014ee87bdfeb62edcc0fb3e3968ee1
linkang <jimao08@sina.com>
2016-10-09 15:59:58 +0800

    ddd

---
+++ demo2/src/main/java/Demo1.java
@@ -0,0 +1,10 @@
+import org.apache.commons.lang3.StringUtils;
+
+/**
+ * Created by linkang on 10/9/16.
+ */
+public class Demo1 {
+    public static void main(String[] args) {
+        System.out.println(StringUtils.isBlank(null));
+    }
+}
