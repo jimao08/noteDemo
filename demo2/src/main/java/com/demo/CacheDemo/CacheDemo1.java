8887e05ec514a870578ae97da1fdf7d4a2165555
linkang <jimao08@sina.com>
2016-11-09 14:49:01 +0800

    update

---
+++ demo2/src/main/java/com/demo/CacheDemo/CacheDemo1.java
@@ -0,0 +1,12 @@
+package com.demo.CacheDemo;
+
+import com.google.common.cache.Cache;
+
+/**
+ * Created by linkang on 10/27/16.
+ */
+public class CacheDemo1 {
+    public static void main(String[] args) {
+
+    }
+}
--- demo2/src/main/java/com/demo/LambdaDemo/Lambda1.java
+++ demo2/src/main/java/com/demo/LambdaDemo/Lambda1.java
@@ -1,8 +1,10 @@
 package com.demo.LambdaDemo;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.Date;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * Created by linkang on 10/19/16.
@@ -16,5 +18,12 @@ public class Lambda1 {
         Integer[] ints = {12, 3, 43, 4, 34, 5, 6};
         Arrays.stream(ints).map(ival -> ival++);
         Arrays.stream(ints).forEach(System.out::println);
+
+        List<Integer> list1 = Arrays.stream(ints).filter(value -> value > 10).collect(Collectors.toList());
+
+
+
+        System.out.println(list1);
+
     }
 }
