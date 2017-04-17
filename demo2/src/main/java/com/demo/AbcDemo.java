58744881e37adc1c55de7b06b935da90d0fb3b6e
linkang <jimao08@sina.com>
2017-04-17 12:09:30 +0800

    abc

---
+++ demo2/src/main/java/com/demo/AbcDemo.java
@@ -0,0 +1,10 @@
+package com.demo;
+
+/**
+ * Created by linkang on 17-4-17.
+ */
+public class AbcDemo {
+    public static void main(String[] args) {
+        System.out.println("hello ");
+    }
+}
