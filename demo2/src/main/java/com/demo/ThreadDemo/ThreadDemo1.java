74f84ecb92f577956424f32f919689d97585eee7
linkang <jimao08@sina.com>
2017-06-12 14:16:27 +0800

    AtomicInteger

---
+++ demo2/src/main/java/com/demo/ThreadDemo/ThreadDemo1.java
@@ -0,0 +1,56 @@
+package com.demo.ThreadDemo;
+
+import org.junit.Test;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ *
+ * 测试AtomicInteger
+ * Created by linkang on 17-6-7.
+ */
+public class ThreadDemo1 {
+    public static int total;
+    public static Object obj = new Integer(123);
+
+    public static void main(String[] args) throws Exception {
+
+        long startTime = System.currentTimeMillis();
+        ThreadDemo1 test = new ThreadDemo1();
+        test.total = new Integer(0);
+
+        Thread t1 = new Thread(new ThreadTest());
+        Thread t2 = new Thread(new ThreadTest());
+        Thread t3 = new Thread(new ThreadTest());
+
+        t1.start();
+        t2.start();
+        t3.start();
+
+        t1.join();
+        t2.join();
+        t3.join();
+
+        System.out.println(test.total);
+
+        long endTime = System.currentTimeMillis();
+        System.out.println("use time=" + (endTime - startTime));
+    }
+
+
+    static class ThreadTest implements Runnable {
+
+        @Override
+        public void run() {
+            for (int i = 0; i < 5000000; i++) {
+//                    test.total.incrementAndGet();
+                synchronized (obj) {
+                    ThreadDemo1.total++;
+                }
+//                    System.out.println(Thread.currentThread().getName() + ":" + test.total.incrementAndGet());;
+            }
+        }
+    }
+}
+
+
