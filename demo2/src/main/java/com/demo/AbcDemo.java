c6bde9027f0beeb648742062ae66591542958310
linkang <jimao08@sina.com>
2017-06-13 17:16:17 +0800

    注解测试

--- demo2/src/main/java/com/demo/AbcDemo.java
+++ demo2/src/main/java/com/demo/AbcDemo.java
@@ -5,6 +5,13 @@ package com.demo;
  */
 public class AbcDemo {
     public static void main(String[] args) {
+        Integer ival = 128;
+        Integer ival2 = 128;
+        System.out.println(ival == ival2);
+
+        Integer iva3 = 127;
+        Integer ival4 = 127;
+        System.out.println(iva3 == ival4);
         System.out.println("hello ");
     }
 }
