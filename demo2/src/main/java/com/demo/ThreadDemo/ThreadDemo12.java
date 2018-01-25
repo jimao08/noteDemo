459d08ecc5ebe116b8d41f779406916b21135652
linkang <linkang@yooshu.cn>
2018-01-26 01:25:04 +0800

    CyclicBarrier demo0

--- demo2/src/main/java/com/demo/ThreadDemo/ThreadDemo12.java
+++ demo2/src/main/java/com/demo/ThreadDemo/ThreadDemo12.java
@@ -1,18 +1,51 @@
 package com.demo.ThreadDemo;
 
-public class ThreadDemo12 {
+import java.util.Random;
+import java.util.concurrent.*;
 
-    public static void main(String[] args) throws InterruptedException {
-        Runtime.getRuntime().addShutdownHook(new Thread(new Runnable() {
+public class ThreadDemo12 {
+    private static CyclicBarrier barrier;
+
+    public static void main(String[] args) {
+        barrier = new CyclicBarrier(3, new Runnable() {
             @Override
             public void run() {
-                System.out.println("hello");
+                System.out.println("======== all threads have arrived at the checkpoint ===========");
             }
-        }));
+        });
+        ExecutorService service = Executors.newCachedThreadPool();
+        service.submit(new Traveler("Traveler1"));
+        service.submit(new Traveler("Traveler2"));
+        service.submit(new Traveler("Traveler3"));
+        service.shutdown();
+    }
 
+    private static class Traveler implements Runnable {
+        private final String name;
 
-        Thread.sleep(1000);
+        Traveler(String name) {
+            this.name = name;
+        }
 
-        System.out.println("end.");
+        @Override
+        public void run() {
+            try {
+                Random rand = new Random();
+                TimeUnit.SECONDS.sleep(rand.nextInt(5));
+                System.out.println(name + " arrived at Beijing.");
+                barrier.await();
+                TimeUnit.SECONDS.sleep(rand.nextInt(5));
+                System.out.println(name + " arrived at Shanghai.");
+                barrier.await();
+                TimeUnit.SECONDS.sleep(rand.nextInt(5));
+                System.out.println(name + " arrived at Guangzhou.");
+                barrier.await();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } catch (BrokenBarrierException e) {
+                e.printStackTrace();
+            }
+
+        }
     }
 }
