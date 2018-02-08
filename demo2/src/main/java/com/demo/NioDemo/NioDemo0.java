63b61a901633a9f7c92a36d72100ebaee8eb2444
linkang <linkang@yooshu.cn>
2018-02-09 02:28:01 +0800

    demo

--- demo2/src/main/java/com/demo/NioDemo/NioDemo0.java
+++ demo2/src/main/java/com/demo/NioDemo/NioDemo0.java
@@ -1,8 +1,11 @@
 package com.demo.NioDemo;
 
+import org.apache.commons.io.FileUtils;
+
 import java.io.*;
 import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel;
+import java.util.Arrays;
 
 public class NioDemo0 {
 
@@ -30,23 +33,49 @@ public class NioDemo0 {
 //            System.out.println(bf.remaining());
 //        }
 
+        final String s = FileUtils.readFileToString(new File("C:\\Users\\Administrator\\workspace\\noteDemo\\demo2\\pom.xml"));
 
-        FileOutputStream fos = new FileOutputStream(file, true);
+
+        FileOutputStream fos = new FileOutputStream(file);
         FileChannel channel = fos.getChannel();
 
-        ByteBuffer bf = ByteBuffer.allocate(64);
-        bf.put("hello world".getBytes());
+        ByteBuffer bf = ByteBuffer.allocate(10);
 
-        bf.flip();
+
+        StringBuilder text = new StringBuilder();
+        for (int i = 0; i < 20000; i++) {
+//            text.append(s);
+        }
+
+        byte[] bytes = "hello".getBytes();
+        System.out.println(bytes.length);
+
+        long stime = System.currentTimeMillis();
+//        for (byte b : bytes) {
+//            bf.put(b);
+//            bf.flip();
+//            channel.write(bf);
+//            bf.clear();
+//        }
+
+
+        bf = ByteBuffer.wrap(bytes);
+        bytes[0] = 'x';
+
+        int count = 0;
         while (bf.hasRemaining()) {
             channel.write(bf);
+            count++;
         }
+
+        System.out.println(count);
 
         channel.close();
         fos.flush();
         fos.close();
 
 
+        System.out.println("utime=" + (System.currentTimeMillis() - stime));
 
     }
 }
